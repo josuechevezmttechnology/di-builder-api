47e30da9900d0afdc115e82c81dd071fb895ee27
josuechevez1998 <josuechevez977@gmail.com>
2026-03-16 13:46:52 -0600

    fix: repracion de ibex crud

--- config/crud.php
+++ config/crud.php
@@ -29,7 +29,7 @@ return [
     */
 
     // 'stub_path' => 'default',
-    'stub_path' => resource_path('stubs/')
+    'stub_path' => resource_path('stubs/'),
 
     /*
     |--------------------------------------------------------------------------
